c15763ddbea0cbd321492b1de32618c0184f713e
sarit-zombie <57765371+sarit-zombie@users.noreply.github.com>
2021-01-06 11:10:13 +0530

    Add files via upload
    This is the shit I made.

---
+++ shit.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class bullshit
+{
+	public:
+	int small_pp;
+	bullshit(int whjr)
+	{
+		small_pp=whjr*0;
+	}
+	int small_shit ()
+	{
+		if (small_pp == 0)
+		{
+			return 1;
+		}
+	}
+};
+
+int main ()
+{
+	bullshit pp(1);
+	int whjr_is_shit = pp.small_shit();
+	if (whjr_is_shit == 0)
+		cout<<"Hello World";
+	else
+	    cout<<"Hello World";
+	return 0;
+}
+
+/* I did not feel like doing anything else to the program cause life is shit and I am busy watching Khalifa.
+I am too tired to code shit and waste time.
+Coding is nothing but the work of a depressed soul.
+PHP sucks !!!*/ 
